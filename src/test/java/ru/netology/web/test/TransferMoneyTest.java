a728631649052e677be80f826b9537c942325f1f
dmitriyneto <qima10021981@gmail.com>
2026-03-23 20:15:13 -0700

    First commit

--- src/test/java/ru/netology/web/test/TransferMoneyTest.java
+++ src/test/java/ru/netology/web/test/TransferMoneyTest.java
@@ -23,10 +23,6 @@ public class TransferMoneyTest {
         verificationPage.validVerify(verificationCode);
     }
 
-    @AfterEach
-    void tearDown() {
-        closeWindow();
-    }
 
     @Test
     public void shouldTransferMoneyFromCardOneToCardTwo() {
